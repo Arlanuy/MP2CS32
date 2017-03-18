6046d841112ebaafd5cb181c7ceea65f394defa5
Arlan Vincent Uy <arlanvincentuy@gmail.com>
2017-03-18 08:57:25 +0000

    level order print finished

--- for_text_twist_games.c
+++ for_text_twist_games.c
@@ -5,15 +5,12 @@
 #define LINELENGTH 100
 #define INPUTADJUSTPREORDER 10
 #define INPUTADJUSTINORDER 9
+#define TRUE 1
+#define FALSE 0
 
 typedef struct Node Node;
 
-struct Node
-{
-    int data;
-    Node *LSON;
-    Node *RSON;
-};
+
     /**
     int filename_size = 50;
     char* input_name = NULL;
@@ -21,10 +18,85 @@ struct Node
     assert(input_name != NULL);
     input_name[strlen(input_name) - 1] = '\0';
     free(input_name);*/
+
+struct Node
+{
+    char data;
+    Node *LSON;
+    Node *RSON;
+};
+
+typedef struct{
+    int front;
+    int rear;
+    Node** arr; 
+}Queue;
+
+void initQueue(Queue* q, int size) {
+    q->front = 0;
+    q->rear = 0;
+    q->arr = malloc(size * sizeof(Node*));
+}
+
+int isEmpty(Queue q) {
+    if (q.front == q.rear) {
+        return TRUE;
+    }
+    
+    else {
+        return FALSE;
+    }
+}
+
+void enqueue(Queue* q, Node* node) {
+    //printf("%d", q->front);
+    q->front++;
+    q->arr[q->front] = node;
+}
+
+Node* dequeue(Queue* q) {
+    q->rear++;
+    return q->arr[q->rear];
+}
+
+void levelOrderPrinting(Node* root, int size) {
+    if (root == NULL) {
+        printf("Null binary tree");
+    }
+    
+    else  {
+        Queue* q = malloc(sizeof(Queue));
+        assert(q != NULL);
+        initQueue(q, size);
+        enqueue(q, root);
+        printf("level order print\n");
+        while (!isEmpty(*q)) {
+            //printf("front %d rear %d", q.front, q.rear);
+            Node* current = dequeue(q);
+            assert(current != NULL);
+            //printf("a %c\t", current);
+            Node* root_LSON = current->LSON;
+
+            Node* root_RSON = current->RSON;
+           
+            if (root_LSON != NULL) {
+                //printf("data LSON %c\n", root_LSON->data);
+                enqueue(q, root_LSON);
+            }
+            
+            if (root_RSON != NULL) {
+                //printf("data RSON %c\n", root_RSON->data);
+                enqueue(q, root_RSON);
+            }
+            free(current);
+        }
+        free(q);
+    }
+}
     
 int main(void) {
     int ind_preorder = 0;
-    Node *root;
+    Node *root = malloc(sizeof(Node));
     int filename_size = 50, string_size = 40;
     char* input_name = NULL;
     input_name = malloc(sizeof(char) * filename_size);
@@ -53,6 +125,21 @@ int main(void) {
          printf("%s\n", preorder_string);
          printf("%s\n", inorder_string); */
     }
+    Node *root_LSON = malloc(sizeof(Node));
+    Node *root_RSON = malloc(sizeof(Node));
+    root->data = 'L';
+    root_LSON->data = 'A';
+    root_RSON->data = 'C';
+    
+    root_LSON->LSON = NULL;
+    root_LSON->RSON = NULL;
+    root_RSON->LSON = NULL;
+    root_RSON->RSON = NULL;
+    
+    root->LSON = root_LSON;
+    root->RSON = root_RSON;
+    levelOrderPrinting(root, string_size);
+    
     free(line);
     free(line2);
     return 0;
