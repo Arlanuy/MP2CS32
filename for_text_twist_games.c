1a00d1b946bf6344c78e6d7b3c07b1f5a2fc3054
Arlan Vincent Uy <arlanvincentuy@gmail.com>
2017-03-20 04:22:55 +0000

    finish for height = 2

--- for_text_twist_games.c
+++ for_text_twist_games.c
@@ -2,11 +2,13 @@
 #include <stdlib.h>
 #include <assert.h>
 #include <string.h>
+#include <math.h>
 #define LINELENGTH 100
 #define INPUTADJUSTPREORDER 10
 #define INPUTADJUSTINORDER 9
 #define TRUE 1
 #define FALSE 0
+#define GOLDENRATIO 1.6180399
 
 typedef struct Node Node;
 
@@ -32,6 +34,7 @@ typedef struct{
     Node** arr; 
 }Queue;
 
+//levelOrderPrinting helpers
 void initQueue(Queue* q, int size) {
     q->front = 0;
     q->rear = 0;
@@ -59,7 +62,68 @@ Node* dequeue(Queue* q) {
     return q->arr[q->rear];
 }
 
-void levelOrderPrinting(Node* root, int size) {
+void printContent(Queue* q) {
+    int i;
+    for (i = 1; i <= q->front; i++) {
+        fprintf(stderr, "pc: %c\t", q->arr[i]->data); 
+    }
+}
+
+void freeContent(Queue* q) {
+    int i;
+    for (i = q->front + 1; i <= q->rear; i++) {
+        free(q->arr[i]); 
+    }
+    free(q->arr);
+}
+
+void tabsPrinter(int num_tabs, FILE* output_file) {
+    int i;
+    for (i = 0; i < num_tabs; i++) {
+        fprintf(output_file, "\t");
+    }
+}
+
+void spacesPrinter(double fractional_tabs, FILE* output_file) {
+    int i, space_tab_equiv = 4;
+    for (i = 0; i < (int)fractional_tabs * space_tab_equiv; i++) {
+        fprintf(output_file, " ");
+    }
+}
+
+int* nullNodeChecker(Queue* q, int num_nodes) {
+    int* nodes_height_tags = malloc((q->front - 1) * sizeof(int));
+    int i, j = 0;
+    for (i = 1; i <= q->front; i++) {
+        fprintf(stderr, "q->arr[i]->data: %c\n", q->arr[i]->data);
+        if (q->arr[i]->LSON == NULL || q->arr[i]->LSON->data == '0') {
+            nodes_height_tags[j] = 0;
+            fprintf(stderr, "j is %d data %c entered at 1\n", j, q->arr[i]->data);
+        }
+        
+        else {
+            nodes_height_tags[j] = 1;
+            fprintf(stderr, "j is %d data %c entered at 2\n", j, q->arr[i]->data);
+        }
+        
+        j++;
+        
+        if (q->arr[i]->RSON == NULL || q->arr[i]->RSON->data == '0') {
+            nodes_height_tags[j] = 0;
+            fprintf(stderr, "j is %d data %c entered at 3\n", j, q->arr[i]->data);
+        }
+        
+        else {
+            nodes_height_tags[j] = 1;
+            fprintf(stderr, "j is %d data %c entered at 4\n", j, q->arr[i]->data);
+        }
+        
+        j++;
+    }
+    return nodes_height_tags;
+}
+
+void levelOrderPrinting(Node* root, int size, FILE* output_file) {
     if (root == NULL) {
         printf("Null binary tree");
     }
@@ -69,41 +133,242 @@ void levelOrderPrinting(Node* root, int size) {
         assert(q != NULL);
         initQueue(q, size);
         enqueue(q, root);
-        printf("level order print\n");
+        double num_tabs, fractional_tabs;
+        int num_nodes = 1, span_tabs = 8, tabs_generator = span_tabs, new_line_generator = 0, null_node_counter = 0; 
+        Node* null_node = malloc(sizeof(Node));
+        assert(null_node != NULL);
+        null_node->data = '0';
+        null_node->LSON = NULL;
+        null_node->RSON = NULL;
+        int nht_iterator = 0;
         while (!isEmpty(*q)) {
             //printf("front %d rear %d", q.front, q.rear);
             Node* current = dequeue(q);
             assert(current != NULL);
-            //printf("a %c\t", current);
-            Node* root_LSON = current->LSON;
 
+            Node* root_LSON = current->LSON;
             Node* root_RSON = current->RSON;
-           
-            if (root_LSON != NULL) {
-                //printf("data LSON %c\n", root_LSON->data);
+            int * nodes_height_tags;
+            //in charge with constructing the graphical binary tree
+            if (current->data != '0') {
+                 
+                if (num_nodes != 1 && new_line_generator == 0) {
+                    nodes_height_tags = nullNodeChecker(q, num_nodes);
+                    int k;
+                    printContent(q);
+                    for (k = 0; k < q->front; k++) {
+                        fprintf(stderr, "k is %d overjoyed %d\t", k, nodes_height_tags[k]);    
+                    }
+                    fprintf(stderr,"\n");
+                    if (num_nodes ==  2) {
+                        if (nodes_height_tags[nht_iterator] == 1) {
+                            if (current->LSON != NULL)
+                            fprintf(stderr, "you can do this node lson %c entered at 1\n", current->LSON->data);
+                            tabsPrinter(((tabs_generator/GOLDENRATIO)*3)/2, output_file);
+                            fprintf(output_file, "\t\t\t/");
+                            nht_iterator++;
+                            if (nodes_height_tags[nht_iterator] == 1) {
+                                tabsPrinter((tabs_generator/GOLDENRATIO)/2, output_file);
+                                fprintf(output_file, "\t\\\n");
+                                if (current->RSON != NULL)
+                                fprintf(stderr, "node rson %c entered at 2\n", current->RSON->data);
+                                tabsPrinter(((tabs_generator/GOLDENRATIO)*3)/2, output_file);
+                                
+                                fprintf(output_file, "\t\t/");
+                                tabsPrinter((tabs_generator/GOLDENRATIO)/2, output_file);
+                                fprintf(output_file, "\t\\\n");
+                            }
+                            else {
+                                tabsPrinter((tabs_generator/GOLDENRATIO)/2, output_file);
+                                fprintf(output_file, "\t \n");
+                                if (current->RSON != NULL)
+                                fprintf(stderr, "node rson %c entered at 3", current->RSON->data);
+                                tabsPrinter(((tabs_generator/GOLDENRATIO)*3)/2, output_file);
+                                fprintf(output_file, "\t\t/");
+                                tabsPrinter((tabs_generator/GOLDENRATIO)/2, output_file);
+                                fprintf(output_file, "\t \n");
+                            }
+                            
+                        }
+                        
+                        else {
+                            //fprintf(stderr, "node lson %c entered at 4", current->LSON->data);
+                            tabsPrinter(((tabs_generator/GOLDENRATIO)*3)/2, output_file);
+                            fprintf(output_file, "\t\t\t ");
+                            nht_iterator++;
+                            if (nodes_height_tags[nht_iterator + 1] == 1) {
+                                tabsPrinter((tabs_generator/GOLDENRATIO)/2, output_file);
+                                fprintf(output_file, "\t\\\n");
+                                if (current->RSON != NULL)
+                                fprintf(stderr, "node rson %c entered at 5\n", current->RSON->data);
+                                tabsPrinter(((tabs_generator/GOLDENRATIO)*3)/2, output_file);
+                                fprintf(output_file, "\t\t ");
+                                tabsPrinter((tabs_generator/GOLDENRATIO)/2, output_file);
+                                fprintf(output_file, "\t\\\n");
+                            }
+                            
+                            else {
+                                tabsPrinter((tabs_generator/GOLDENRATIO)/2, output_file);
+                                fprintf(output_file, "\t \n");
+                                if (current->RSON != NULL)
+                                fprintf(stderr, "node rson %c entered at 6\n", current->RSON->data);
+                                tabsPrinter(((tabs_generator/GOLDENRATIO)*3)/2, output_file);
+                                fprintf(output_file, "\t\t ");
+                                tabsPrinter((tabs_generator/GOLDENRATIO)/2, output_file);
+                                fprintf(output_file, "\t \n");
+                                
+                            }
+                             
+                        }
+                        nht_iterator++;
+                        
+                    }
+
+                    else if (num_nodes == 4){
+                        int j;
+                        
+                        for (j = 1; j < num_nodes; j*=2) {
+                            if (nodes_height_tags[nht_iterator] == 1) {
+                                tabsPrinter(((tabs_generator/GOLDENRATIO)*3)/2, output_file);
+                                fprintf(output_file, "\t\t/");
+                            }
+                            
+                            else  {
+                                tabsPrinter(((tabs_generator/GOLDENRATIO)*3)/2, output_file);
+                                fprintf(output_file, "\t\t ");
+                            }
+                            nht_iterator++;
+                            
+                            if (nodes_height_tags[nht_iterator] == 1) {
+                                tabsPrinter((tabs_generator/GOLDENRATIO)/2, output_file);
+                                fprintf(output_file, "\t\t\\");
+                            }
+                            
+                            else {
+                                tabsPrinter((tabs_generator/GOLDENRATIO)/2, output_file);
+                                fprintf(output_file, "\t\t ");
+                            }
+                            nht_iterator++;
+                        }
+                        fprintf(output_file, "\n");
+ 
+                    }
+                    
+                    else {
+                        int j;
+                        for (j = 1; j < num_nodes; j*=2) {
+                            if (nodes_height_tags[nht_iterator] == 1) {
+                                spacesPrinter(((fractional_tabs/GOLDENRATIO)*3)/2, output_file);
+                                fprintf(output_file, "/");
+                            }
+                            
+                            else {
+                                spacesPrinter(((fractional_tabs/GOLDENRATIO)*3)/2, output_file);
+                                fprintf(output_file, "/");
+                            }
+                            nht_iterator++;
+                            if (nodes_height_tags[nht_iterator] == 1) {
+                                spacesPrinter((fractional_tabs/GOLDENRATIO)/2, output_file);
+                                fprintf(output_file, "\\\n");
+                            }
+                            
+                            else {
+                                spacesPrinter((fractional_tabs/GOLDENRATIO)/2, output_file);
+                                fprintf(output_file, "\\\n");
+                            }
+                            nht_iterator++;
+                        }
+                    }
+                   
+                }
+                tabsPrinter(tabs_generator, output_file);
+                fprintf(output_file, "%c\t", current->data);
+                
+            }
+            
+            else {
+                tabsPrinter(tabs_generator, output_file);
+            }
+            
+                       
+            if (root_LSON != NULL && root_LSON->data != '0') {
+                //fprintf(stderr, "data LSON %c\n", root_LSON->data);
+                
                 enqueue(q, root_LSON);
             }
             
-            if (root_RSON != NULL) {
-                //printf("data RSON %c\n", root_RSON->data);
+            else {
+                //tabsPrinter(tabs_generator, output_file);
+                null_node_counter++;
+                enqueue(q, null_node);
+                //fprintf(stderr, "data LSON %c\n", null_node->data);
+                
+            }
+            
+            if (root_RSON != NULL && root_RSON->data != '0') {
+                //fprintf(stderr, "data RSON %c\n", root_RSON->data);
                 enqueue(q, root_RSON);
             }
-            free(current);
+            
+            else {
+                //tabsPrinter(tabs_generator, output_file);
+                null_node_counter++;
+                enqueue(q, null_node);
+                //fprintf(stderr, "data RSON %c\n", null_node->data);
+            }
+            //times 2 because we are checking for the number of null nodes on the current height + 1
+            if (null_node_counter == 2*num_nodes) {
+                //fprintf(stderr, "nlg %d current data %c nnc: %d != 2*num_nodes: %d", new_line_generator, current->data, null_node_counter, 2*num_nodes);
+                fprintf(output_file, "\n");
+                break;
+            }
+            
+            new_line_generator++;
+            if (new_line_generator == num_nodes) {
+                fractional_tabs = (double)tabs_generator/GOLDENRATIO;
+                //fprintf(stderr, "ft: %f", fractional_tabs);
+                num_tabs = round(fractional_tabs);
+                tabs_generator = (int)num_tabs;
+                fprintf(output_file, "\n");
+                /**
+                fprintf(output_file, "\n");
+                tabsPrinter(tabs_generator, output_file);
+                fprintf(output_file, "\t  /");
+                tabsPrinter((tabs_generator * 9)/4, output_file);
+                fprintf(output_file, "\\ \n");
+                tabsPrinter(tabs_generator * 2, output_file);
+                fprintf(output_file, "\t\t  /\n");
+                tabsPrinter((tabs_generator*6)/4, output_file);
+                fprintf(output_file, "  \\ \n"); */
+                num_nodes *= 2;
+                new_line_generator = 0;
+                //new_line_generator = null_node_counter;
+                null_node_counter = 0;
+                
+            }
+            
         }
+        free(null_node);
+        freeContent(q);
         free(q);
     }
 }
+
     
 int main(void) {
     int ind_preorder = 0;
     Node *root = malloc(sizeof(Node));
     int filename_size = 50, string_size = 40;
-    char* input_name = NULL;
-    input_name = malloc(sizeof(char) * filename_size);
-    assert(input_name != NULL);
-    input_name = "input.txt";
+    //char* input_name = malloc(sizeof(char) * filename_size);
+    //assert(input_name != NULL);
+    char* input_name = "input.txt";
     FILE* input_file = fopen(input_name, "r");
     assert(input_file != NULL);
+    //char* output_name = malloc(sizeof(char) * filename_size);
+    //assert(output_name != NULL);
+    char* output_name = "output.txt";
+    FILE* output_file = fopen(output_name, "a");
+    assert(output_file != NULL);
     char* line = malloc(LINELENGTH * sizeof(char));
     assert(line != NULL);
     char* line2 = malloc(LINELENGTH * sizeof(char));
@@ -125,6 +390,7 @@ int main(void) {
          printf("%s\n", preorder_string);
          printf("%s\n", inorder_string); */
     }
+    
     Node *root_LSON = malloc(sizeof(Node));
     Node *root_RSON = malloc(sizeof(Node));
     root->data = 'L';
@@ -137,10 +403,31 @@ int main(void) {
     root_RSON->RSON = NULL;
     
     root->LSON = root_LSON;
-    root->RSON = root_RSON;
-    levelOrderPrinting(root, string_size);
+    //root->RSON = root_RSON;
     
+    Node *root2_LSON = malloc(sizeof(Node));
+    Node *root2_RSON = malloc(sizeof(Node));
+    root2_LSON->data = 'U';
+    root2_RSON->data = 'F';
+    
+    root_LSON->LSON = root2_LSON;
+    root_LSON->RSON = root2_RSON;
+    root2_RSON->RSON = root_RSON;
+    root2_RSON->LSON = NULL;
+    root2_LSON->LSON = NULL;
+    root2_LSON->RSON = NULL;
+    //printf("love %p-%p", root2_LSON->LSON, root2_LSON->RSON);
+
+    
+    levelOrderPrinting(root, string_size, output_file);
+    free(root);
+    free(root_RSON);
+    free(root_LSON);
+    free(root2_RSON);
+    free(root2_LSON);
     free(line);
     free(line2);
+    free(input_file);
+    free(output_file);
     return 0;
 }
